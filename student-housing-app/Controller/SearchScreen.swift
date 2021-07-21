950124cd5eaaf3b154a6f6c067d829ffd454dd1a
Azim Jiwani <azimjiwani@hotmail.com>
2021-07-21 01:16:29 -0400

    Create functional location search bar

--- student-housing-app/Controller/SearchScreen.swift
+++ student-housing-app/Controller/SearchScreen.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
-class SearchScreen: UIViewController {
+class SearchScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate {
     
-    var resultSearchController:UISearchController? = nil
+    var dataArray = [String]()
+    var filteredArray = [String]()
+    var shouldShowSearchResults = false
+    var tblSearchResults = UITableView()
+    var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +24,12 @@ class SearchScreen: UIViewController {
     func setupView (){
         backBtn()
         titleLabel()
+        configureSearchController()
+        citiesTable()
+        loadListOfCities()
     }
     
     @objc func backBtnPressed(){
-        let searchScreen = SearchScreen()
-        searchScreen.title="Search Rentals"
         self.navigationController?.popViewController(animated: true)
     }
     
@@ -57,9 +62,89 @@ class SearchScreen: UIViewController {
             titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 60)
         ])
-        titleLabel.text = "Search a city"
+        titleLabel.text = "Search"
         titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
         titleLabel.textColor = UIColor.black
     }
-
+    
+    func citiesTable(){
+        tblSearchResults.register(UITableViewCell.self, forCellReuseIdentifier: "idCell")
+        tblSearchResults.dataSource = self
+        tblSearchResults.delegate = self
+        view.addSubview(tblSearchResults)
+        tblSearchResults.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tblSearchResults.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 90),
+            tblSearchResults.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tblSearchResults.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tblSearchResults.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+    
+    func loadListOfCities() {
+        if let filepath = Bundle.main.path(forResource: "cities", ofType: "txt") {
+            let citiesString = try? String(contentsOfFile: filepath)
+            dataArray = citiesString!.components(separatedBy:"\n")
+            tblSearchResults.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if shouldShowSearchResults {
+            return filteredArray.count
+        }
+        else {
+            return dataArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath as IndexPath)
+        
+        if shouldShowSearchResults {
+            cell.textLabel?.text = filteredArray[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = dataArray[indexPath.row]
+        }
+        return cell
+    }
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        tblSearchResults.tableHeaderView = searchController.searchBar
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tblSearchResults.reloadData()
+    }
+     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tblSearchResults.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tblSearchResults.reloadData()
+        }
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+     
+        filteredArray = dataArray.filter({ (city) -> Bool in
+            let cityText: NSString = city as NSString
+            let range = cityText.range(of: searchString!, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+     tblSearchResults.reloadData()
+    }
 }
